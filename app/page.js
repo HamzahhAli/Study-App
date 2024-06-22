97d0bd92ed2fb8102a0460bf63b9d008e9cb6cbe
HamzahhAli <139597923+HamzahhAli@users.noreply.github.com>
2024-06-22 12:34:53 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Button, Modal, Form, Card, Container, Row, Col } from 'react-bootstrap';
